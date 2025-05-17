586bac7e31ebbf9c51fae0d43394a1a4dbe134e3
Roffe-K <simonsson1234@gmail.com>
2025-05-17 19:22:29 +0200

    Add files via upload

--- src/components/DrinksSection.tsx
+++ src/components/DrinksSection.tsx
@@ -45,7 +45,7 @@ const DrinksSection = () => {
           </p>
         </div>
         
-        <div className="space-y-6">
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
           {drinks.map(drink => (
             <DrinkCard key={drink.id} drink={drink} />
           ))}
