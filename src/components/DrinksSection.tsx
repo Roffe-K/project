a113e95adc6af35818cd0645a38f49574495ffca
Roffe-K <simonsson1234@gmail.com>
2025-05-17 20:26:58 +0200

    Add files via upload

--- src/components/DrinksSection.tsx
+++ src/components/DrinksSection.tsx
@@ -8,25 +8,22 @@ const drinks = [
     title: 'Dyngnacke',
     description: 'Namnet säger allt. Du är så trött på hela skiten nu. Den här veckan har varit kass och nu får det fan räcka. Du tänker inte lyssna på några jävla råd. I kväll är det en all out, så jävla enkelt är det. 70% valfri vodka. 30% sprite. ',
     ingredients: ['7 delar valfri vodka', '3 delar sprite'],
-    preparationTime: '5 min',
     alcoholic: true,
     image: 'https://cdn.discordapp.com/attachments/458008375536517120/1373361716729090178/IMG_3244.jpg?ex=682a2245&is=6828d0c5&hm=b1bf2952f448ce9adbd69a7f5fa52879af5e8f1678318776b6af4186e44c63b3&'
   },
   {
     id: 2,
     title: 'Lidingö Ice Tea',
-    description: 'A delicious non-alcoholic blend of mixed berries and citrus.',
-    ingredients: ['Mixed berries', 'Lemon juice', 'Soda water', 'Sugar syrup', 'Mint'],
-    preparationTime: '5 min',
-    alcoholic: false,
+    description: 'Ett måste för alla som gillar iste. Häll ihop allting över en näve is. Börja med fun lighten, lägg på en citronskiva och servera.',
+    ingredients: ['2 delar Ljusrom', '1 del Fun Light Lemonade', '1 del Iste', '1 del Schweppes Lemon', '1 skiva Citron'],
+    alcoholic: true,
     image: 'https://images.pexels.com/photos/5947182/pexels-photo-5947182.jpeg'
   },
   {
     id: 3,
     title: 'Klitoribbean',
-    description: 'The timeless Cuban highball with rum, mint, and lime.',
-    ingredients: ['White rum', 'Mint leaves', 'Lime juice', 'Sugar', 'Soda water'],
-    preparationTime: '8 min',
+    description: 'En riktigt exotisk stänkare, rör försiktigt med is. Sätt ananasskivan på kanten utav glaset och njut. ',
+    ingredients: ['1 del Captain Morgan', '1 del Nocco Caribbean', '3 delar Ananasjuice', '1 Ananasskiva'],
     alcoholic: true,
     image: 'https://images.pexels.com/photos/4021983/pexels-photo-4021983.jpeg'
   }
