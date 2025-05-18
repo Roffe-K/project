04bb21305bfbdb518900fe8e311252f3f2900a6a
Roffe-K <simonsson1234@gmail.com>
2025-05-18 11:58:31 +0200

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import fs from 'fs';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+  server: {
+    host: true,
+    port: 5173,
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, 'cert/server.key')),
+      cert: fs.readFileSync(path.resolve(__dirname, 'cert/server.crt')),
+    },
   },
 });
