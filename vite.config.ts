d6155e57cc7e7a5477d1c133e1b5444682143a82
Roffe-K <simonsson1234@gmail.com>
2025-05-18 12:15:10 +0200

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,5 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import fs from 'fs';
-import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,9 +10,6 @@ export default defineConfig({
   server: {
     host: true,
     port: 5173,
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, 'cert/server.key')),
-      cert: fs.readFileSync(path.resolve(__dirname, 'cert/server.crt')),
-    },
+    https: false, // ⛔ stäng av internt https
   },
 });
