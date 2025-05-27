a99c2e0aa46ec5730b7c1097e1f568d46065f027
Roffe-K <simonsson1234@gmail.com>
2025-05-27 14:36:44 +0200

    Add files via upload

--- src/pages/DrinksPage.tsx
+++ src/pages/DrinksPage.tsx
@@ -42,7 +42,7 @@ const DrinksPage: React.FC = () => {
   const categories = ['all', ...new Set(allDrinks.map(drink => drink.category || 'uncategorized'))];
 
   return (
-    <div className="min-h-screen bg-gradient-to-b from-gray-900 to-gray-800">
+    <div className="py-16 px-4 bg-gradient-to-br from-gray-900 via-purple-900 to-gray-900">
       {/* Header */}
       <div className="bg-gradient-to-r from-purple-900 to-purple-700 py-20 px-4">
         <div className="container mx-auto text-center">
